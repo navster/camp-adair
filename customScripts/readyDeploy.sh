9e54f5a99312696a17dcb9405fa323d51efc7b30
Navishkar Rao <nav.rao@icloud.com>
2012-11-05 23:17:28 +1300

    readying for app fog deployment

--- customScripts/readyDeploy.sh
+++ customScripts/readyDeploy.sh
@@ -1,4 +1,4 @@
-cp ./sites/default/phpfog.php ./sites/default/settings.php
+sudo cp ./sites/default/phpfog.php ./sites/default/settings.php
 #make sure everything is commited
 git add .
 git commit -a -m "readying for app fog deployment"
