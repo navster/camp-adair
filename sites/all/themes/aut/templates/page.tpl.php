d4970a0d6e737942a0e4e350884f4c5f6e240d48
Navishkar Rao <nav.rao@icloud.com>
2012-11-13 09:10:40 +1300

    moving elements into separate php files

--- sites/all/themes/aut/templates/page.tpl.php
+++ sites/all/themes/aut/templates/page.tpl.php
@@ -1,31 +1,6 @@
 <div class='row' id='wrapper'>
 <div class='twelve columns'>
-  <div class='row'>
-    <div class="twelve columns">
-      <nav class="top-bar">
-        <ul>
-          <!-- Title Area -->
-          <li class='name'><h1><a href='#'><?php print $site_name; ?></a></h1></li>
-          <li class="toggle-topbar"><a href="#"></a></li>
-        </ul>
-
-        <section>
-          <!-- Left Nav Section -->
-          <!--<ul class="left">
-            <?php print render($main_menu_expanded); ?>
-          </ul>-->
-          <ul class='left'>
-            <li class="divider"></li>
-            <li><a href='#'>Home</a></li>          
-            <li><a href='#'>Activities</a></li>
-            <li><a href='#'>Group Bookings</a></li>
-            <li><a href='#'>Services and Facilities</a></li>
-            <li><a href='#'>Make a Booking</a></li>
-          </ul>
-        </section>
-      </nav>
-    </div>
-  </div>
+  <?php include 'includes/page-menu.php'; ?>
 
   <div class="row">
     <div class="twelve columns">
@@ -34,7 +9,6 @@
         <img src="<?php echo base_path() . path_to_theme() ?>/images/slide2.jpg" />
         <img src="<?php echo base_path() . path_to_theme() ?>/images/slide3.jpg" />
       </div>
-
       <hr />
     </div>
   </div>  
