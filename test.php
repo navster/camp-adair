62c2a1cff8879c4818e0f9f5f79247c7ac0574bd
Navishkar Rao <nav.rao@icloud.com>
2012-11-13 07:20:48 +1300

    quick db deployment script in ruby for easily creating settings.php depending on chosen environment

---
+++ test.php
@@ -0,0 +1,26 @@
+  <?php
+  $databases = array (
+    'default' => 
+    array (
+      'default' => 
+      array (
+        'database' => 'drupal',
+        'username' => 'root',
+        'password' => 'root',
+        'host' => 'localhostssss',
+        'port' => '',
+        'driver' => 'mysql',
+        'prefix' => '',
+      ),
+    ),
+  );
+  $update_free_access = FALSE;
+  $drupal_hash_salt = 'Wp5UFccJZ2E7GWJvOpAgYgVctRALw-hAvMvI3FJTRWY';
+  ini_set('session.gc_probability', 1);
+  ini_set('session.gc_divisor', 100);
+  ini_set('session.gc_maxlifetime', 200000);
+  ini_set('session.cookie_lifetime', 2000000);
+
+  $conf['404_fast_paths_exclude'] = '//(?:styles)//';
+  $conf['404_fast_paths'] = '/.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
+  $conf['404_fast_html'] = '<html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
